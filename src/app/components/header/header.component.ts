ecb3086b6cc61f1e2df11a3306825ae01e86f721
abluvin <153931513+abluvin@users.noreply.github.com>
2026-06-14 22:17:31 +0200

    Correction d'un problème avec la l'etat de connexion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(public authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
-
 }
